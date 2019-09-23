5aa234bf71e1853debd491b39ca1b1e70483da08
Adam Quigley <adamjohnquigley@gmail.com>
2019-09-24 09:07:51 +1000

    add lineup

--- waihekejazzfestival/src/App.js
+++ waihekejazzfestival/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Footer from "./components/Footer"
+import Lineup from "./components/Lineup"
 import SponsorSection from "./components/SponsorSection"
 
 const App = () => {
     return (
         <div className="App">
+            <Lineup/>
             <SponsorSection/>
             <Footer />
         </div>
---
+++ waihekejazzfestival/src/components/Lineup.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Lineup = () => {
+    return (
+        <section id="lineup" className="section-lineup">
+            <div className="row">
+                <h3>Lineup</h3>
+                <div className="lineup-artists">
+                <div className="first-tier"><p>Trinity Roots</p></div>
+                <div className="second-tier"><p>Louana Whitney | MC Tali</p></div>
+                <div className="second-tier"><p>Hopetoun Brown | Frank Booker</p></div>
+                <div className="third-tier"><p>Gypsy Kings Tribute Band | Royal Jackets</p></div>
+                <div className="third-tier"><p>Wellington City Shake-em-on-downers</p></div>
+                <div className="third-tier"><p>Nairobi Trio | A Tribute to Tom Waits</p></div>
+                <div className="third-tier"><p>George Washingmachine | Kate Wadey Trio</p></div>
+                <div className="third-tier"><p>The Phil Edwards Band | Gitbox Rebellion</p></div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Lineup;
