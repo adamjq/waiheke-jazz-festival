705fd2252bc9a731d5086b24216424d6a5427cb3
Adam Quigley <adam.quigley@versent.com.au>
2019-12-14 10:45:02 +1100

    add nav bar

--- waihekejazzfestival/src/components/Header.js
+++ waihekejazzfestival/src/components/Header.js
@@ -3,18 +3,11 @@ import React from 'react';
 
 import NavBar from "./NavBar"
 
-// const LOGO = "./img/banner-love-square.png"
 const LOGO = "./img/jazz-fest-banner-generic.png"
-// const LOGO = "./img/eastern-banner-sherpa.png"
-// const LOGO = "./img/jazz-fest-2020-logo-eastern-blue.png"
-// const LOGO = "./img/jazz-fest-2020-logo-eastern-crop.png"
-// const LOGO = "./img/eastern-banner.png"
-// const LOGO = "./img/wai-jam-2020.png"
-
 const Header = () => {
     return (
         <header>
-            {/* <NavBar/> */}
+            <NavBar/>
 
             <div className="hero-text-box">
                 <img className="logo-img" src={LOGO} alt="Waiheke Jazz Festival Logo"/>
