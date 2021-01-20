f6f70cf2dc6bb8aa7b0f5430241ba00702081d41
Adam Quigley <adamquigley@Adams-MacBook-Pro.local>
2021-01-20 17:47:07 +1100

    add auckland city council logo

--- waihekejazzfestival/src/components/SponsorSection.js
+++ waihekejazzfestival/src/components/SponsorSection.js
@@ -19,9 +19,10 @@ const SponsorSection = () => {
         </div> */}
 
         <div className="row">
-            {/* <SponsorImage image="PerpetualGuardian.png" alt="Perpetual Guardian Logo"/>
-            <SponsorImage image="AucklandCouncilLogo.png" alt="Auckland Council Logo" /> */}
+            {/* <SponsorImage image="PerpetualGuardian.png" alt="Perpetual Guardian Logo"/> */}
             <SponsorImage image="Fullers360Logo.png" alt="Fullers 360 Logo"/>
+            <br></br><br></br>
+            <SponsorImage image="AucklandCouncilLogo.png" alt="Auckland Council Logo" />
             {/* <SponsorImage image="LogoMusicPlanet.png" alt="Music Planet Logo"/>
             <SponsorImage image="PostageStampWinesLogo.png" alt="Postage Stamp Wines Logo"/> */}
         </div>
