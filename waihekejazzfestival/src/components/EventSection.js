b76beea84124157796bbe5fae2ca28ec6cd8126d
Adam Quigley <adamjohnquigley@gmail.com>
2021-03-15 22:16:26 +1100

    update fringe show time

--- waihekejazzfestival/src/components/EventSection.js
+++ waihekejazzfestival/src/components/EventSection.js
@@ -91,6 +91,18 @@ const EventSection = () => {
             { <h3>Fringe</h3> }
 
             <EventCard  
+                link="https://www.casitamiro.co.nz/shop/product/482170/1-good-friday-jazz--mireya-ramos-trio--2nd-april/"
+                image="fringe_2.png"
+                alt='Mireya Ramos Trio'
+                name='Mireya Ramos Trio'
+                date="Friday 2 April"
+                time="7 - 11pm"
+                location="Casita Miro"
+                description={['Born in Puerto Rico, from the age of 17 Mireya paid her dues playing Mariachi, salsa, merengue, jazz, R&B and hip-hop. Mireya takes us through the Latin-tinged Americana landscape of the US, featuring classic Jazz as well as the Mexican tradition.',<br/>,<br/>,<strong>Ticket price includes 3 waves of Casita's famous tapas</strong> ]}
+                artists="Mireya Ramos Trio"
+                artistsSmall="Mireya Ramos Trio"
+            />
+            <EventCard  
                 link="http://www.iticket.co.nz/events/2021/apr/wijf-weirder-side"
                 image="fringe_1.png"
                 alt='The Weirder Side of Jazz'
@@ -101,18 +113,6 @@ const EventSection = () => {
                 description={['Hop aboard the Jazzabout bus for a magical mystery tour to experience the "Death of Love Part 2" - unusual versions of music from Tom Waits and Nick Cave, along with performances from special guests.',<br/>,<br/>,<strong>PICK-UP 1pm sharp & DROP OFF 3pm outside the Library, Oneroa</strong> ]}
                 artists="Johnny Fleury | Floss Fleury"
                 artistsSmall={["Johnny Fleury", <br/>, "Floss Fleury"]}
-            />
-            <EventCard  
-                link="https://www.casitamiro.co.nz/shop/product/482170/1-good-friday-jazz--mireya-ramos-trio--2nd-april/"
-                image="fringe_2.png"
-                alt='Mireya Ramos Trio'
-                name='Mireya Ramos Trio'
-                date="Saturday 3 April"
-                time="7 - 11pm"
-                location="Casita Miro"
-                description={['Born in Puerto Rico, from the age of 17 Mireya paid her dues playing Mariachi, salsa, merengue, jazz, R&B and hip-hop. Mireya takes us through the Latin-tinged Americana landscape of the US, featuring classic Jazz as well as the Mexican tradition.',<br/>,<br/>,<strong>Ticket price includes 3 waves of Casita's famous tapas</strong> ]}
-                artists="Mireya Ramos Trio"
-                artistsSmall="Mireya Ramos Trio"
             />
 
         </section>
