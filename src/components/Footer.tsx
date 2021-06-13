a780f09550a84bba9f97f7812c5dd6e89f9c6949
Adam Quigley <adamjohnquigley@gmail.com>
2021-06-13 15:59:57 +1200

    feat: remove old sections

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-    return (
-        <footer>
-        <div className="row">
-            <p className="footer-text"> &copy; Copyright 2021. All rights reserved.</p>
-        </div>
-        </footer>
-    );
-}
